d639f582a2e5745e523e27acb411288f4182abf2
BathspaJohnAndreFederiso <71628636+BathspaJohnAndreFederiso@users.noreply.github.com>
2022-01-17 13:48:03 +0400

    updated some stuff

--- Data Driven App/dataDrivenApplication/src/ofApp.cpp
+++ Data Driven App/dataDrivenApplication/src/ofApp.cpp
@@ -2,7 +2,29 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	//loads images declared in header, with image assets from src folder of this project
+	testImg.load("ned.jpg");
 
+	// loads fonts, same as the image above but for fonts (styles, sizes)
+	smallTxt.load("Dongle-Light.ttf", 23);
+	medTxt.load("Dongle-Regular.ttf", 25);
+	bigTxt.load("RobotoCondensed-Regular.ttf", 29);
+	uiHeader.load("Dongle-Bold.ttf", 55); 
+
+	//button setting
+	btn1.set(10, 70, 350, 60); // 10 and 50 as x and y coordinates, 350 and 60 as length and width
+	btn2.set(10, 140, 350, 60); // same except the y value is incremented by 70 to arrange in a column
+	btn3.set(10, 210, 350, 60);
+	btn4.set(10, 280, 350, 60);
+	btn5.set(10, 350, 350, 60);
+	btn6.set(10, 420, 350, 60);
+	btn7.set(10, 490, 350, 60);
+	btn8.set(10, 560, 350, 60);
+	btn9.set(10, 630, 350, 60);
+	btn10.set(10, 700, 350, 60);
+
+	queWindow.set(0, 60, 370, 760); // num are as follows, XYLW 
+	resWindow.set(390, 20, 615, 725);
 }
 
 //--------------------------------------------------------------
@@ -12,6 +34,36 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofBackground(108, 184, 235); // sets color to lightish blue
+	//(108, 184, 235);
+	
+
+	ofSetColor(228, 241, 255); // sets color to offwhite
+	ofDrawRectangle(queWindow);
+
+	ofSetColor(101, 119, 134); // sets color to gray
+	ofDrawRectangle(resWindow); // draws resWindow as a rectangle
+
+	ofDrawRectangle(btn1); 
+	ofDrawRectangle(btn2);
+	ofDrawRectangle(btn3);
+	ofDrawRectangle(btn4);
+	ofDrawRectangle(btn5);
+	ofDrawRectangle(btn6);
+	ofDrawRectangle(btn7);
+	ofDrawRectangle(btn8);
+	ofDrawRectangle(btn9);
+	ofDrawRectangle(btn10);
+
+	ofSetColor(255, 255, 255);
+	uiHeader.drawString("Tweet Search", 30, 45); // uses uiHeader object for drawString function, Tweet Search string, 30 x and 45 y added as values
+	uiHeader.drawString("Count Tweets", 35, 115); 
+	uiHeader.drawString("Count Tweets", 35, 185);
+	//testImg.draw(600, 500, 212, 212);
+}
+
+//--------------------------------------------------------------
+void ofApp::loadData() {
 
 }
 
--- Data Driven App/dataDrivenApplication/src/ofApp.h
+++ Data Driven App/dataDrivenApplication/src/ofApp.h
@@ -8,6 +8,7 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+		void loadData();
 
 		void keyPressed(int key);
 		void keyReleased(int key);
@@ -20,5 +21,11 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+
+		string ofApp::wrapString(string text, int width); // wrapString function declared
+
+		ifstream data; // declares ifstream data for file handling the twitter csv file
+		ofImage testImg; // declares ofImage testImg for testing images
+		ofRectangle btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, queWindow, resWindow; // declares ofRectangle objects btn1-10 and resWindow
+		ofTrueTypeFont smallTxt, medTxt, bigTxt, boldTxt, uiHeader; // declares four ofTrueTypeFont
 };
