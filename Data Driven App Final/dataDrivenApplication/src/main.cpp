d2d524b7418b27e3d96e4294344783a111a07d01
BathspaJohnAndreFederiso <71628636+BathspaJohnAndreFederiso@users.noreply.github.com>
2022-01-19 16:03:49 +0400

    spellcheck
    flase to false

--- Data Driven App Final/dataDrivenApplication/src/main.cpp
+++ Data Driven App Final/dataDrivenApplication/src/main.cpp
@@ -5,7 +5,7 @@
 int main( ){
 
     ofGLFWWindowSettings settings; // declares ofGLFWWindowSettings settings 
-    settings.resizable = false; // sets resizable to flase
+    settings.resizable = false; // sets resizable to false
     settings.setSize(1024, 768); // sets the size of settings to 1024 length and 768 height
     ofCreateWindow(settings); // uses ofCreateWindow function with settings passed in 
     return ofRunApp(new ofApp); // returns ofRunApp 
